3adb58115d13723eb2d5c4aac80b27aacd06e4c1
James Booth <boothj5@gmail.com>
2011-08-11 01:35:43 +0100

    Shuffle cards

--- c-head.c
+++ c-head.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include "c-head.h" 
 
 void create_players(player **, int) ;
 void populate_players(player *, int) ;
+void shuffle(card *, size_t) ;
 
 int main(void)
 {
@@ -25,6 +27,10 @@ int main(void)
 
     show_players(players, num_players) ;
     show_deck(deck, total_cards_needed) ;
+    shuffle(deck, total_cards_needed) ;
+    newline() ;
+    show_deck(deck, total_cards_needed) ;
+
 }
 
 void create_players(player **players, int num_players)
@@ -42,5 +48,21 @@ void populate_players(player *players, int num_players)
     {
         request_player_name(name, i+1) ;
         players[i] = make_player(name, HUMAN) ;
+    }
+}
+
+void shuffle(card *deck, size_t n)
+{
+    if (n > 1) 
+    {
+        srand((unsigned)time(NULL)) ;
+        size_t i;
+        for (i = 0; i < n - 1; i++) 
+        {
+          size_t j = i + rand() / (RAND_MAX / (n - i) + 1);
+          card t = deck[j];
+          deck[j] = deck[i];
+          deck[i] = t;
+        }
     }
 }
