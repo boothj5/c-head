9aa2b4e572442239e9cb09da767c5761ee5840c9
James Booth <boothj5@gmail.com>
2011-08-12 16:29:32 +0100

    Whitespace

--- c-head.c
+++ c-head.c
@@ -21,8 +21,8 @@ int main(void)
     calc_cards_required(&game) ;
     create_deck(&game) ;
     create_players(game.players, game.num_players) ;
-
     shuffle(&game) ;
+
     newline() ;
     printf("Initial deck\n") ;
     show_cards(game.deck, game.deck_size) ;
