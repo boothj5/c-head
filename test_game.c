5320705e58c92757a2effafc09244f3a5f5421fd
James Booth <boothj5@gmail.com>
2011-09-29 23:25:10 +0100

    Refactor of game test

--- test_game.c
+++ test_game.c
@@ -19,6 +19,28 @@ static void setup(void)
     player = &game.players[game.current_player] ;
 }
 
+static void test_valid_card_on_card(struct card_t test_card, struct card_t pile_card)
+{
+    setup() ;
+    num_choices = 1 ;
+    game.pile[game.pile_size++] = pile_card;
+    player->hand[player->hand_size++] = test_card ;
+    choices[0] = 0 ;
+
+    assert_true(valid_move(game, choices, num_choices)) ;
+}
+
+static void test_not_valid_card_on_card(struct card_t test_card, struct card_t pile_card)
+{
+    setup() ;
+    num_choices = 1 ;
+    game.pile[game.pile_size++] = pile_card;
+    player->hand[player->hand_size++] = test_card ;
+    choices[0] = 0 ;
+
+    assert_false(valid_move(game, choices, num_choices)) ;
+}
+
 void test_not_valid_move_no_cards(void)
 {
     setup() ;
@@ -33,7 +55,6 @@ void test_not_valid_more_cards_than_hand(void)
 {
     setup() ;
     num_choices = 2 ;
-
     game.pile[game.pile_size++] = make_card(THREE, HEARTS) ;
     player->hand[player->hand_size++] = make_card(FOUR, SPADES) ;
     choices[0] = 0 ;
@@ -46,8 +67,6 @@ void test_not_valid_when_rank_not_equal(void)
 {   
     setup() ;
     num_choices = 2 ;
-
-    game.pile[game.pile_size++] = make_card(THREE, HEARTS) ;
     player->hand[player->hand_size++] = make_card(FOUR, SPADES) ;
     player->hand[player->hand_size++] = make_card(SEVEN, DIAMONDS) ;
     choices[0] = 0 ;
@@ -60,7 +79,6 @@ void test_not_valid_when_not_in_hand(void)
 {   
     setup() ;
     num_choices = 2 ;
-
     game.pile[game.pile_size++] = make_card(THREE, HEARTS) ;
     player->hand[player->hand_size++] = make_card(FOUR, SPADES) ;
     player->hand[player->hand_size++] = make_card(SEVEN, DIAMONDS) ;
@@ -72,43 +90,27 @@ void test_not_valid_when_not_in_hand(void)
 
 void test_valid_four_on_three(void)
 {
-    setup() ;
-    num_choices = 1 ;
-
-    game.pile[game.pile_size++] = make_card(THREE, HEARTS) ;
-    player->hand[player->hand_size++] = make_card(FOUR, SPADES) ;
-    choices[0] = 0 ;
-
-    assert_true(valid_move(game, choices, num_choices)) ;
+    test_valid_card_on_card(make_card(FOUR, SPADES), make_card(THREE, HEARTS)) ; 
 }
 
 void test_valid_three_on_three(void)
 {
-    setup() ;
-    num_choices = 1 ;
-
-    game.pile[game.pile_size++] = make_card(THREE, HEARTS) ;
-    player->hand[player->hand_size++] = make_card(THREE, SPADES) ;
-    choices[0] = 0 ;
-
-    assert_true(valid_move(game, choices, num_choices)) ;
+    test_valid_card_on_card(make_card(THREE, SPADES), make_card(THREE, HEARTS)) ; 
 }
 
 void test_not_valid_three_on_four(void)
 {
-    setup() ;
-    num_choices = 1 ;
-
-    game.pile[game.pile_size++] = make_card(FOUR, HEARTS) ;
-    player->hand[player->hand_size++] = make_card(THREE, SPADES) ;
-    choices[0] = 0 ;
-
-    assert_false(valid_move(game, choices, num_choices)) ;
+    test_not_valid_card_on_card(make_card(THREE, SPADES), make_card(FOUR, HEARTS)) ; 
 }
 
 void test_valid_two_on_nine(void)
 {
-    
+    test_valid_card_on_card(make_card(TWO, DIAMONDS), make_card(NINE, SPADES)) ; 
+}
+
+void test_valid_seven_on_queen(void)
+{
+    test_valid_card_on_card(make_card(SEVEN, DIAMONDS), make_card(QUEEN, SPADES)) ; 
 }
 
 void register_game_tests(void)
@@ -121,5 +123,7 @@ void register_game_tests(void)
     add_test(test_valid_four_on_three, "test_valid_four_on_three" ) ;
     add_test(test_valid_three_on_three, "test_valid_three_on_three" ) ;
     add_test(test_not_valid_three_on_four, "test_not_valid_three_on_four" ) ;
+    add_test(test_valid_two_on_nine, "test_valid_two_on_nine" ) ;
+    add_test(test_valid_seven_on_queen, "test_valid_seven_on_queen" ) ;
 }
 
