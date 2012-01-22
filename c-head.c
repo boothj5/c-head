96f462e9632dde7e2e68d00ee27fd8cc09e5ab31
James Booth <boothj5@gmail.com>
2012-01-22 03:38:36 +0000

    card and c-head tidy up

--- c-head.c
+++ c-head.c
@@ -6,111 +6,118 @@
 #include "game_rules.h"
 #include "console.h"
 
-static void perform_swap(struct game_t *game) ;
-static void perform_last_cards_move(struct game_t *game) ;
-static void perform_move(struct game_t *game) ;
+static void perform_swap(struct game_t *game);
+static void perform_last_cards_move(struct game_t *game);
+static void perform_move(struct game_t *game);
 
 int main(void)
 {
-    struct game_t game ;
-    int nplayers, ncards ;
-    char names[MAX_NUM_PLAYERS][MAX_NAME_LEN] ;
-    struct player_t shithead ;
+    struct game_t game;
+    int nplayers, ncards;
+    char names[MAX_NUM_PLAYERS][MAX_NAME_LEN];
+    struct player_t shithead;
     
-    clearscreen() ;
-    show_welcome_msg() ;
-    nplayers = request_num_players() ;
-    ncards = request_num_cards_each() ;
-    request_player_names(names, nplayers) ;
-    game = make_game(nplayers, names, ncards) ;
-    init_game(&game) ;
-    perform_swap(&game) ;
-    first_move(&game) ;
-    clearscreen() ;
-    show_game_summary(game) ;
+    clearscreen();
+    show_welcome_msg();
+    nplayers = request_num_players();
+    ncards = request_num_cards_each();
+    request_player_names(names, nplayers);
+    game = make_game(nplayers, names, ncards);
+    init_game(&game);
+    perform_swap(&game);
+    first_move(&game);
+    clearscreen();
+    show_game_summary(game);
 
     while (continue_play(game))
         if (player_on_last_cards(&game))
-            perform_last_cards_move(&game) ;
+            perform_last_cards_move(&game);
         else
-            perform_move(&game) ;
+            perform_move(&game);
 
-    shithead = get_shithead(game) ;
-    show_shithead(shithead) ;
+    shithead = get_shithead(game);
+    show_shithead(shithead);
 
-    return 0 ;
+    return 0;
 }
 
 static void perform_last_cards_move(struct game_t *game)
 {
-    int face_down_choice ;
-    face_down_choice = request_face_down_move(game->players[game->current_player]) ;
+    int face_down_choice;
+    struct player_t *player = &game->players[game->current_player]; 
+
+    face_down_choice = request_face_down_move(*player);
+
     if (can_lay_from_face_down(*game, face_down_choice-1)) {
-        show_can_move_from_face_down(game->players[game->current_player].face_down[face_down_choice-1]) ;
-        wait_user() ;
-        make_move_from_face_down(game, face_down_choice-1) ;
-        clearscreen() ;
-        show_game_summary(*game) ;
-        move_to_next_player(game) ;
+        show_can_move_from_face_down(player->face_down[face_down_choice-1]);
+        wait_user();
+        make_move_from_face_down(game, face_down_choice-1);
+        clearscreen();
+        show_game_summary(*game);
+        move_to_next_player(game);
     } else {
-        show_pickup_from_face_down(game->players[game->current_player].face_down[face_down_choice-1]) ;
-        wait_user() ;
-        pick_up_pile_and_face_down(game, face_down_choice-1) ;
-        clearscreen() ;
-        show_game_summary(*game) ;
-        move_to_next_player(game) ;
+        show_pickup_from_face_down(player->face_down[face_down_choice-1]);
+        wait_user();
+        pick_up_pile_and_face_down(game, face_down_choice-1);
+        clearscreen();
+        show_game_summary(*game);
+        move_to_next_player(game);
     }
 }
 
 static void perform_move(struct game_t *game)
 {
-    int card_choices[20] ;
-    int num_choices = 0 ;
+    int card_choices[20];
+    int num_choices = 0;
+    struct player_t *player = &game->players[game->current_player];
+
     if (can_move(*game)) {
-        newline() ;
-        request_move(game->players[game->current_player], card_choices, &num_choices) ;
+        newline();
+        request_move(*player, card_choices, &num_choices);
+
         if (valid_move(*game, card_choices, num_choices)) {
-            make_move(game, card_choices, num_choices) ;
-            num_choices = 0 ;
-            clearscreen() ;
-            show_game_summary(*game) ;
-            move_to_next_player(game) ;
+            make_move(game, card_choices, num_choices);
+            num_choices = 0;
+            clearscreen();
+            show_game_summary(*game);
+            move_to_next_player(game);
         } else {
-            num_choices = 0 ;
-            show_bad_move() ;
+            num_choices = 0;
+            show_bad_move();
         }
     } else {
-        show_pickup(game->players[game->current_player].name) ;
-        wait_user() ;
-        pick_up_pile(game) ;
-        num_choices = 0 ;
-        clearscreen() ;
-        show_game_summary(*game) ;
-        move_to_next_player(game) ;
+        show_pickup(player->name);
+        wait_user();
+        pick_up_pile(game);
+        num_choices = 0;
+        clearscreen();
+        show_game_summary(*game);
+        move_to_next_player(game);
     }
 }
 
 static void perform_swap(struct game_t *game)
 {
-    int i, do_swap, hand_choice, face_up_choice ;
-    struct player_t *player ;
+    int i, do_swap, hand_choice, face_up_choice;
+    struct player_t *player;
 
-    for (i = 0 ; i < game->num_players ; i++) {
-        player = &game->players[i] ;
-        clearscreen() ;
-        show_player(*player) ;
-        do_swap = request_swap_cards(player->name) ;
+    for (i = 0; i < game->num_players; i++) {
+        player = &game->players[i];
+        clearscreen();
+        show_player(*player);
+        do_swap = request_swap_cards(player->name);
+
         while (do_swap) {
-            clearscreen() ;
-            show_player(*player) ;
-            newline() ;
-            hand_choice = request_hand_swap(player->hand_size) ;
-            face_up_choice = request_faceup_swap(player->face_up_size) ;
-            swap(player, hand_choice, face_up_choice) ;
-            clearscreen() ;
-            show_player(*player) ;
-            newline() ;
-            do_swap = request_swap_more(player->name) ;
+            clearscreen();
+            show_player(*player);
+            newline();
+            hand_choice = request_hand_swap(player->hand_size);
+            face_up_choice = request_faceup_swap(player->face_up_size);
+            swap(player, hand_choice, face_up_choice);
+            clearscreen();
+            show_player(*player);
+            newline();
+            do_swap = request_swap_more(player->name);
        }
     }
 }
--- card.c
+++ card.c
@@ -4,72 +4,70 @@
 
 struct card_t make_card(const enum cardrank rank, const enum cardsuit suit)
 {
-    struct card_t card ;
-    card.rank = rank ;
-    card.suit = suit ;
-    return card ;
+    struct card_t card;
+    card.rank = rank;
+    card.suit = suit;
+    return card;
 }
 
 struct card_t find_lowest_card(const struct card_t *cards, const int num_cards)
 {
-    struct card_t lowest = cards[0] ;
+    struct card_t lowest = cards[0];
     int i ;
-    for (i = 1 ; i < num_cards; i++)
+
+    for (i = 1; i < num_cards; i++)
         if (card_cmp(&cards[i], &lowest) < 0)
-            lowest = cards[i] ;
-    return lowest ;
+            lowest = cards[i];
+
+    return lowest;
 }
 
 int special_card(const struct card_t card)
 {
     switch(card.rank) {
-    case TWO:
-        return TRUE ;
-    case SEVEN:
-        return TRUE ;
-    case TEN:
-        return TRUE ;
-    default:
-        return FALSE ;
+    case TWO:   return TRUE;
+    case SEVEN: return TRUE;
+    case TEN:   return TRUE;
+    default:    return FALSE;
     }
 }
 
 int card_cmp(const void *card1, const void *card2)
 {   
-    const struct card_t *icard1 = (const struct card_t *) card1 ;
-    const struct card_t *icard2 = (const struct card_t *) card2 ;
+    const struct card_t *icard1 = (const struct card_t *) card1;
+    const struct card_t *icard2 = (const struct card_t *) card2;
 
     if (special_card(*icard1) && special_card(*icard2))
-        return EQ ;
+        return EQ;
     else if (special_card(*icard1) && !special_card(*icard2))
-        return GT ;
+        return GT;
     else if (special_card(*icard2))
-        return LT ;
+        return LT;
     else if (icard1->rank == icard2->rank)
-        return EQ ;
+        return EQ;
     else if (icard1->rank < icard2->rank)
-        return LT ;
+        return LT;
     else
         return GT ;
 }
 
 int cards_equal(const struct card_t card1, const struct card_t card2)
 {
-    return ((card1.rank == card2.rank) && (card1.suit == card2.suit)) ;
+    return ((card1.rank == card2.rank) && (card1.suit == card2.suit));
 }
 
 int ranks_equal(const struct card_t card1, const struct card_t card2)
 {
-    return ((card1.rank == card2.rank)) ;
+    return ((card1.rank == card2.rank));
 }
 
 int all_ranks_equal(const struct card_t *cards, const int num_cards)
 {
-    int i = 0 ;
+    int i = 0;
 
-    for (i = 1 ; i < num_cards ; i++)
+    for (i = 1; i < num_cards; i++)
         if (!ranks_equal(cards[i], cards[0]))
-            return FALSE ;
+            return FALSE;
 
     return TRUE ;
 }
@@ -77,59 +75,42 @@ int all_ranks_equal(const struct card_t *cards, const int num_cards)
 char* show_rank(const struct card_t card)
 {
     switch(card.rank) {
-    case TWO:
-        return "TWO" ;
-    case THREE:
-        return "THREE" ;
-    case FOUR:
-        return "FOUR" ;
-    case FIVE:
-        return "FIVE" ;
-    case SIX:
-        return "SIX" ;
-    case SEVEN:
-        return "SEVEN" ;
-    case EIGHT:
-        return "EIGHT" ;
-    case NINE:
-        return "NINE" ;
-    case TEN:
-        return "TEN" ;
-    case JACK:
-        return "JACK" ;
-    case QUEEN:
-        return "QUEEN" ;
-    case KING:
-        return "KING" ;
-    case ACE:
-        return "ACE" ;
-    default:
-        return "ERROR!!!" ;
+    case TWO:   return "TWO";
+    case THREE: return "THREE";
+    case FOUR:  return "FOUR";
+    case FIVE:  return "FIVE";
+    case SIX:   return "SIX";
+    case SEVEN: return "SEVEN";
+    case EIGHT: return "EIGHT";
+    case NINE:  return "NINE";
+    case TEN:   return "TEN";
+    case JACK:  return "JACK";
+    case QUEEN: return "QUEEN";
+    case KING:  return "KING";
+    case ACE:   return "ACE";
+    default:    return "ERROR!!!";
     }
 }
 
 char* show_suit(const struct card_t card)
 {
     switch(card.suit) {
-    case HEARTS:
-        return "HEARTS" ;
-    case DIAMONDS:
-        return "DIAMONDS" ;
-    case CLUBS:
-        return "CLUBS" ;
-    case SPADES:
-        return "SPADES" ;
-    default:
-        return "ERROR!!!!" ;
+    case HEARTS:   return "HEARTS";
+    case DIAMONDS: return "DIAMONDS";
+    case CLUBS:    return "CLUBS";
+    case SPADES:   return "SPADES";
+    default:       return "ERROR!!!!";
     } 
 }
 
-void add_card_to_cards(struct card_t cards[], int *num_cards, const struct card_t card)
+void add_card_to_cards(struct card_t cards[], int *num_cards, 
+    const struct card_t card)
 {
     cards[(*num_cards)++] = card;
 }
 
-void remove_card_from_cards(struct card_t cards[], int *num_cards, const struct card_t card)
+void remove_card_from_cards(struct card_t cards[], int *num_cards, 
+    const struct card_t card)
 {
     int i, j;
     for(i = 0;i < *num_cards;i++) {
@@ -143,6 +124,6 @@ void remove_card_from_cards(struct card_t cards[], int *num_cards, const struct 
 }
 
 void sort_cards(struct card_t *cards, const int num_cards) {
-    qsort(cards, num_cards, sizeof(struct card_t), card_cmp) ;
+    qsort(cards, num_cards, sizeof(struct card_t), card_cmp);
 }
 
